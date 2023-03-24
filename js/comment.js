2fdb88762eb076ed490d656cce2703ac1af83344
Kuza_Ko <cveta-mavk@mail.ru>
2023-03-24 20:39:22 +0800

    refactoring comments

--- js/comment.js
+++ js/comment.js
@@ -1,14 +1,13 @@
 import { getTemplate } from './util.js';
 
-const COMMENT_PACK_SIZE = 5;
+const PACK_SIZE = 5;
 
-const commentsLoaderButton = document.querySelector('.comments-loader');
-const CommentsCount = document.querySelector('.social__comment-count');
+const loadButton = document.querySelector('.comments-loader');
+const countElement = document.querySelector('.social__comment-count');
 const commentsContainer = document.querySelector('.social__comments');
 const templateComment = getTemplate('usersComments');
 
-let onMoreCommentsButtonClick = null;
-let showedCommentAmount = 0;
+let savedComments = [];
 
 const renderComment = ({ avatar, name, message }) => {
 	const сommentElement = templateComment.cloneNode(true);
@@ -22,38 +21,35 @@ const renderComment = ({ avatar, name, message }) => {
 	return сommentElement;
 };
 
+const onLoadButtonClick = () => {
+	const showedAmount = commentsContainer.children.length;
+	let endOfPack = showedAmount + PACK_SIZE;
+	const isAllCommentsShow = endOfPack >= savedComments.length;
+
+	endOfPack = isAllCommentsShow ? savedComments.length : endOfPack;
+
+	const slicedComments = savedComments.slice(showedAmount, endOfPack);
+
+	const commentFragment = document.createDocumentFragment();
+	for (const comment of slicedComments) {
+		commentFragment.appendChild(renderComment(comment));
+	}
+	commentsContainer.appendChild(commentFragment);
+
+	countElement.textContent = `${endOfPack} из ${savedComments.length} комментариев`;
+
+	loadButton.hidden = isAllCommentsShow;
+};
+
+loadButton.addEventListener('click', onLoadButtonClick);
+
 const renderPhotoComments = (comments) => {
-	onMoreCommentsButtonClick = () => {
-		let endOfPack = showedCommentAmount + COMMENT_PACK_SIZE;
-		const isAllCommentsShow = endOfPack >= comments.length;
-
-		endOfPack = isAllCommentsShow ? comments.length : endOfPack;
-
-		const slicedComments = comments.slice(showedCommentAmount, endOfPack);
-
-		const commentFragment = document.createDocumentFragment();
-
-		for (const comment of slicedComments) {
-			commentFragment.appendChild(renderComment(comment));
-		}
-
-		CommentsCount.textContent = `${endOfPack} из ${comments.length} комментариев`;
-
-		commentsContainer.appendChild(commentFragment);
-		showedCommentAmount = endOfPack;
-
-		commentsLoaderButton.hidden = isAllCommentsShow;
-	};
-
-	commentsLoaderButton.addEventListener('click', onMoreCommentsButtonClick);
-	commentsLoaderButton.click();
+	savedComments = comments;
+	loadButton.click();
 };
 
 const clearComments = () => {
 	commentsContainer.innerHTML = '';
-	showedCommentAmount = 0;
-
-	commentsLoaderButton.removeEventListener('click', onMoreCommentsButtonClick);
 };
 
 export { renderPhotoComments, clearComments };
