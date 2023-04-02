11518bf5880ac94269f28d734676dd4da993a797
Kuza_Ko <cveta-mavk@mail.ru>
2023-04-02 23:17:48 +0800

    добавляет обработку данных с сервера

--- js/new-photo/index.js
+++ js/new-photo/index.js
@@ -1,10 +1,13 @@
-import { isEscapeKey, toggleModalClasses } from '../util.js';
+import { isEscapeKey, showAlert, toggleModalClasses } from '../util.js';
 import { validate, resetValidation } from './validation.js';
 import { resetScale } from './zoom.js';
 import { photoForm, descriptionElement, hashtagsInput } from './elements.js';
 import { resetEffects } from './effects.js';
+import { sendData } from '../api.js';
 
 const photoModal = photoForm.querySelector('.img-upload__overlay');
+const submitButton = photoForm.querySelector('#upload-submit');
+
 /** @type {HTMLInputElement} */
 const uploadFile = photoForm.filename;
 
@@ -35,7 +38,29 @@ photoForm.addEventListener('reset', () => {
 	resetEffects();
 });
 
-photoForm.addEventListener('submit', (evt) => {
-	evt.preventDefault();
-	validate();
-});
+const blockSubmitButton = () => {
+	submitButton.disabled = true;
+};
+
+const unblockSubmitButton = () => {
+	submitButton.disabled = false;
+};
+
+const setOnFormSubmit = (onSuccess) => {
+	photoForm.addEventListener('submit', (evt) => {
+		evt.preventDefault();
+		// validate();
+		const isValid = validate();
+		if (isValid) {
+			blockSubmitButton();
+			sendData(new FormData(evt.target))
+				.then(onSuccess)
+				.catch((err) => {
+					showAlert(err.message);
+				})
+				.finally(unblockSubmitButton);
+		}
+	});
+};
+
+setOnFormSubmit(closeModal);
--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 /**
  * Функция для проверки длины строки.
  *
@@ -132,6 +134,27 @@ const toggleModalClasses = (modalElement, willBeOpened = true) => {
 	document.body.classList.toggle('modal-open', willBeOpened);
 };
 
+const showAlert = (message) => {
+	const alertContainer = document.createElement('div');
+	alertContainer.style.zIndex = '100';
+	alertContainer.style.position = 'absolute';
+	alertContainer.style.left = '0';
+	alertContainer.style.top = '0';
+	alertContainer.style.right = '0';
+	alertContainer.style.padding = '10px 3px';
+	alertContainer.style.fontSize = '30px';
+	alertContainer.style.textAlign = 'center';
+	alertContainer.style.backgroundColor = 'red';
+
+	alertContainer.textContent = message;
+
+	document.body.append(alertContainer);
+
+	setTimeout(() => {
+		alertContainer.remove();
+	}, ALERT_SHOW_TIME);
+};
+
 export {
 	checkStringLength,
 	isPalindrome,
@@ -143,5 +166,6 @@ export {
 	createRandomIdFromRangeGenerator,
 	isEscapeKey,
 	getTemplate,
-	toggleModalClasses
+	toggleModalClasses,
+	showAlert,
 };
